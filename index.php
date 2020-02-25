117515fbad2fe3bf140e82bfcf4b733aa3598172
IS Glushkov <53116914+GlushkovIS@users.noreply.github.com>
2020-02-25 13:31:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: i.glushkov
+ * Date: 25.02.2020
+ * Time: 12:39
+ */
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<footer>
+    <div class="footer-logo">
+        <img src="https://ld-wp73.template-help.com/wordpress/prod_27609/v1/wp-content/uploads/2020/01/logo.svg"
+             class="jet-logo__img lazyloaded" alt="Electroidens">
+    </div>
+    <div class="footer-tel">
+        <a href="tel:80047591232"> <span class="elementor-icon-list-text">+ 800 4759 1232</span> </a>
+    </div>
+    <div class="footer-address">3674 Harrison Street, San Francisco, 94143,CA, USA.</div>
+
+    <div class="footer-menu-first">
+        <a href="https://ld-wp73.template-help.com/wordpress/prod_27609/v1/"> <span class="elementor-icon-list-text">Home</span>
+        </a>
+        <a href="https://ld-wp73.template-help.com/wordpress/prod_27609/v1/"> <span class="elementor-icon-list-text">About</span>
+        </a>
+        <a href="https://ld-wp73.template-help.com/wordpress/prod_27609/v1/"> <span class="elementor-icon-list-text">Services</span>
+        </a>
+        <a href="https://ld-wp73.template-help.com/wordpress/prod_27609/v1/"> <span class="elementor-icon-list-text">Projects</span>
+        </a>
+        <a href="https://ld-wp73.template-help.com/wordpress/prod_27609/v1/"> <span class="elementor-icon-list-text">Contacts</span>
+        </a>
+    </div>
+    <div class="footer-menu-second">
+        <a href="https://ld-wp73.template-help.com/wordpress/prod_27609/v1/"> <span
+                    class="elementor-icon-list-text">FAQ</span> </a>
+        <a href="https://ld-wp73.template-help.com/wordpress/prod_27609/v1/"> <span class="elementor-icon-list-text">Help Center</span>
+        </a>
+        <a href="https://ld-wp73.template-help.com/wordpress/prod_27609/v1/"> <span class="elementor-icon-list-text">Terms & Privacy</span>
+        </a>
+    </div>
+</footer>
+</body>
+</html>
